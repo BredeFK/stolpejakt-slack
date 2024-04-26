62c71175df12568685747310c09808f82f4a2dd5
Brede Fritjof Klausen <brede@fritjof.no>
2024-04-26 18:54:27 +0200

    Automatically get members from code, and move stolpejakten requests to own file

--- main.py
+++ main.py
@@ -1,41 +1,15 @@
 import json
 import os
-import urllib.parse
 
 import requests
-from Member import Member
 from dotenv import load_dotenv
+from stolpejakten import get_group_member_scoreboard
 
 load_dotenv()
-TOKEN = os.environ.get('TOKEN')
 WEBHOOK_URL = os.environ.get('WEBHOOK_URL')
 
-with open('members.txt', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
+sorted_members = get_group_member_scoreboard()
 
-member_names = [line.strip() for line in lines]
-print(member_names)
-members = []
-
-for name in member_names:
-    urlencoded_name = urllib.parse.quote_plus(name)
-    req = requests.get(url=f'https://apiv10.stolpejakten.no/toplists?type=-1&kommune=0&page=0&search={urlencoded_name}',
-                       headers={'Authorization': f'Bearer {TOKEN}'})
-
-    if req.status_code == 200:
-        body = req.json()
-        if body['count'] == 1:
-            user = body['results'][0]
-            member = Member(user['rank'], user['user_name'], int(user['score']))
-            members.append(member)
-            # print(f'{member.rank} | {member.user_name}: {member.score}')
-        else:
-            members.append(Member('NOT FOUND', name, -1))
-            print(f'Found more than one user with name {name}')
-    else:
-        print(f'Something went wrong: {req.status_code}: {req.text}')
-
-sorted_members = sorted(members, key=lambda m: m.score, reverse=True)
 blocks = {"blocks": [
     {"type": "header", "text": {"type": "plain_text", "text": "Resultater for WoI i Stolpejakten :stolpejakten:"}}]}
 
@@ -70,11 +44,14 @@ info = {
 blocks['blocks'].append(divider)
 blocks['blocks'].append(info)
 
-print(blocks)
+print(json.dumps(blocks))
 
-slack_request = requests.post(url=WEBHOOK_URL, headers={'Content-type': 'application/json'}, data=json.dumps(blocks))
 
-if slack_request.status_code == 200:
-    print('Successfully sent slack message')
-else:
-    print(f'Error[{slack_request.status_code}] sending slack message: {slack_request.text}')
+def post_slack_message(formatted_message):
+    slack_request = requests.post(url=WEBHOOK_URL, headers={'Content-type': 'application/json'},
+                                  data=json.dumps(blocks))
+
+    if slack_request.status_code == 200:
+        print('Successfully sent slack message')
+    else:
+        print(f'Error[{slack_request.status_code}] sending slack message: {slack_request.text}')
---
+++ stolpejakten.py
@@ -0,0 +1,85 @@
+import os
+import urllib.parse
+
+import requests
+from dotenv import load_dotenv
+
+from Member import Member
+
+load_dotenv()
+TOKEN = os.environ.get('TOKEN')
+GROUP_CODE = os.environ.get('GROUP_CODE')
+
+BASE_URL = 'https://apiv10.stolpejakten.no'
+
+
+def get_group_id():
+    req_me = requests.get(url=f'{BASE_URL}/affiliations/me', headers={'Authorization': f'Bearer {TOKEN}'})
+    if req_me.status_code == 200:
+        user_groups = req_me.json()
+        if len(user_groups['results']) != 0:
+            group_id = None
+            for group in user_groups['results']:
+                if group['code'] == GROUP_CODE:
+                    group_id = group['id']
+            if group_id is None:
+                print(f'Error: Group with code {GROUP_CODE} not found')
+            else:
+                print(f'Group with code {GROUP_CODE} found')
+                return group_id
+        else:
+            print(f'Error: user has no groups')
+    else:
+        print(f'Error getting group ID: {req_me.status_code}: {req_me.text}')
+        return None
+
+
+def get_group_member_names():
+    group_id = get_group_id()
+    if group_id is None:
+        exit("Error: GroupId not found")
+
+    req_group = requests.get(url=f'{BASE_URL}/affiliations/members/{group_id}',
+                             headers={'Authorization': f'Bearer {TOKEN}'})
+    if req_group.status_code == 200:
+        group_members = req_group.json()
+        if len(group_members['results']) != 0:
+            name_list = []
+            for member in group_members['results']:
+                name_list.append(member['name'])
+            print(f'Found {len(group_members["results"])} members')
+            return name_list
+        else:
+            print(f'Error: Could not find members for group {group_id}')
+            return None
+    else:
+        print(f'Error getting members for group {group_id}: {req_group.status_code}: {req_group.text}')
+        return None
+
+
+def get_group_member_scoreboard():
+    member_names = get_group_member_names()
+    if member_names is None:
+        exit("Error: Group members not found")
+
+    members = []
+    for name in member_names:
+        urlencoded_name = urllib.parse.quote_plus(name)
+        req_user = requests.get(
+            url=f'{BASE_URL}/toplists?type=-1&kommune=0&page=0&search={urlencoded_name}',
+            headers={'Authorization': f'Bearer {TOKEN}'})
+
+        if req_user.status_code == 200:
+            body = req_user.json()
+            if body['count'] == 1:
+                user = body['results'][0]
+                member = Member(user['rank'], user['user_name'], int(user['score']))
+                members.append(member)
+            else:
+                members.append(Member('NOT FOUND', name, -1))
+                print(f'Found more than one user with name {name}')
+        else:
+            print(f'Something went wrong: {req_user.status_code}: {req_user.text}')
+            return None
+
+    return sorted(members, key=lambda m: m.score, reverse=True)
